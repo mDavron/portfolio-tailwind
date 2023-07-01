16a93eb44ade61c3b53f20dd14b8755bd0e6d4f0
Davron01~ <d.mirabdullayev@mail.ru>
2023-07-01 13:01:41 +0500

    test

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BiHomeAlt, BiUser } from "react-icons/bi";
-import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+import { BsClipboardData, BsBriefcase, BsChatSquareText } from "react-icons/bs";
 import { Link } from "react-scroll";
 
 const Nav = () => {
@@ -9,17 +9,50 @@ const Nav = () => {
       <div className=" container mx-auto ">
         {/* nav inner */}
         <div className="mx-auto flex h-[96px]  w-full max-w-[460px] items-center justify-between rounded-full bg-black/20 px-5 text-2xl text-white/50 backdrop:blur-xl">
-          <Link className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center">
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="home"
+            className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center"
+          >
             <BiHomeAlt />
           </Link>
-          <Link className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center">
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="about"
+            className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center"
+          >
             <BiUser />
           </Link>
-          <Link className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center">
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="services"
+            className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center"
+          >
             <BsClipboardData />
           </Link>
-          <Link className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center">
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="work"
+            className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center"
+          >
             <BsBriefcase />
+          </Link>
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="contact"
+            className="flex h-[60px] w-[60px] cursor-pointer items-center justify-center"
+          >
+            <BsChatSquareText />
           </Link>
         </div>
       </div>
